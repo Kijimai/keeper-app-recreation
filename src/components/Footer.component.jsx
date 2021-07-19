bbd884a70cfd86ffa5a3bbe42154008db9a05e4b
Jibbyworks <JDBucog@yahoo.com>
2021-07-19 16:35:19 -0700

    initial commit

---
+++ src/components/Footer.component.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./footer.style.css"
+
+export const Footer = props => {
+  return (
+    <footer>
+      <div className="footer-links">
+        <ul>
+          <li><a href="https://github.com/Kijimai"><i class="fab fa-github"></i></a></li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/Header.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./header.style.css"
+
+export const Header = props => {
+  return (
+    <header>
+      <h1>Keeper App-eroni</h1>
+    </header>
+  )
+}
